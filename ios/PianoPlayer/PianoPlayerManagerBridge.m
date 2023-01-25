e41058fd88fc3ec745ef4b140db6f2bf716a7fa2
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2023-01-26 02:02:23 +0700

    feat: add comment

--- ios/PianoPlayer/PianoPlayerManagerBridge.m
+++ ios/PianoPlayer/PianoPlayerManagerBridge.m
@@ -1,5 +1,13 @@
 #import <React/RCTBridgeModule.h>
 
+/**
+ * This is the bridge between the native code and the JavaScript code.
+ * The JavaScript code will call the methods defined here.
+ *
+ * @Params
+ * PianoPlayerModule: The name of the module that will be used in the JavaScript code.
+ * PianoPlayerManager: The Swift Class Module Manager.
+ */
 @interface RCT_EXTERN_REMAP_MODULE(PianoPlayerModule, PianoPlayerManager, NSObject)
 
 RCT_EXTERN_METHOD(play:(NSArray *)notes tempo:(nonnull NSNumber *)tempo resolve:(RCTPromiseResolveBlock *)resolve reject:(RCTPromiseRejectBlock *)reject);
