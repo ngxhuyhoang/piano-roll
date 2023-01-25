806a321fddd7b2ef4b6d58619657dc80c78b634f
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2023-01-26 02:14:31 +0700

    comment

--- ios/PianoPlayer/PianoPlayerManagerBridge.m
+++ ios/PianoPlayer/PianoPlayerManagerBridge.m
@@ -8,6 +8,19 @@
  * PianoPlayerModule: The name of the module that will be used in the JavaScript code.
  * PianoPlayerManager: The Swift Class Module Manager.
  */
+
+/**
+ * PianoPlayerManager.h
+ *
+ * This is a module for PianoPlayerManager. It is responsible for initializing the native
+ * PianoPlayerManager object and exposing methods for controlling the native PianoPlayerManager
+ * object.
+ *
+ * PianoPlayerManager is a native singleton that manages a single PianoPlayer object. It
+ * is responsible for initializing the PianoPlayer and exposing methods for controlling
+ * the PianoPlayer.
+ */
+
 @interface RCT_EXTERN_REMAP_MODULE(PianoPlayerModule, PianoPlayerManager, NSObject)
 
 RCT_EXTERN_METHOD(play:(NSArray *)notes tempo:(nonnull NSNumber *)tempo resolve:(RCTPromiseResolveBlock *)resolve reject:(RCTPromiseRejectBlock *)reject);
